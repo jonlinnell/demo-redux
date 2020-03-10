6ef168791043fa64f3a603758e061f89ab26af5a
Jon Linnell <jon.linnell@thirdbridge.com>
2020-03-10 15:03:51 +0000

    Add styles and clean up

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import styles from './index.module.scss';
+
 const Index = () => {
   return (
-    <div>
+    <div className={styles.index}>
       <h1>Hello I am a test</h1>
       <p>ya ya ya I am test ya ya ya ayyy lmao</p>
     </div>
